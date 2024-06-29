a27548f04b563115b3af5e26f76d3e3b3a592bc8
João Jesus <marj@cognizant-mobility.com>
2024-06-29 01:17:53 +0100

    add support for chapters and saving the url and description

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # YouTube Downloader
+A simple Python script to download YouTube videos.
+## Features:
+- Downloads the highest resolution video stream and highest bitrate audio stream and muxes them together.
+- Extracts the video chapters from the description and adds them to the video
+- Saves the video url and description into a text file 
 ## Requirements
 - Install [ffmpeg](https://www.ffmpeg.org/download.html)
+- Install [GPAC](https://gpac.io/downloads/gpac-nightly-builds/)
 - Create a virtual environment: ```python -m venv .venv```   
 - Activate environment:
   - Windows: ```.venv/Scripts/activate```
@@ -9,7 +15,7 @@
 ## Usage
 ```shell
 python main.py "<YouTubeVideoUrl>"
-```
+```~~~~
 example:
 ```shell
 python main.py "https://www.youtube.com/watch?v=cJbvcH0JNGA"
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from pytube import YouTube
-import ffmpeg
-import os
-import sys
+import ffmpeg, os, sys, datetime, re, subprocess
 
 
-def video_audio_mux(path_audio_source: str, path_image_source: str, out_video_path: str) -> None:
+def mux_files(path_audio_source: str, path_image_source: str, path_chapters: str, out_video_path: str) -> None:
     video = ffmpeg.input(path_image_source).video
     audio = ffmpeg.input(path_audio_source).audio
     ffmpeg.output(audio, video, out_video_path, vcodec='copy', acodec='copy').run()
+
+    if path_chapters:
+        add_chapters_to_mp4(path_chapters, out_video_path)
 
 
 def delete_file(file_path: str) -> None:
@@ -17,9 +18,8 @@ def delete_file(file_path: str) -> None:
         print(f'The file "{file_path}" does not exist')
 
 
-def download_streams(url: str) -> (str, str):
-    downloads_path = "/downloads/"
-    output_path = f".{downloads_path}"
+def download_streams(url: str) -> (str, str, str):
+    output_path = "downloads/"
     yt = YouTube(url)
     video_path = (yt
                   .streams
@@ -37,22 +37,89 @@ def download_streams(url: str) -> (str, str):
                     .order_by('bitrate')
                     .desc()
                     .first())
+
+    path = video_path.split(".")[0]
+
     audio_path = (audio_stream
-                  .download(filename=f"{video_path.split(".")[0]}.m4a",
+                  .download(filename=f"{path}.m4a",
                             output_path=output_path))
 
-    return audio_path, video_path
+    write_description_file(path, yt)
+
+    chapters = get_chapters(yt.description)
+
+    chapters_path = ""
+
+    if len(chapters) > 0:
+        chapters_path = f"{video_path.split(".")[0]}_chapter.txt"
+        write_chapters_file(chapters_path, chapters)
+
+    return audio_path, video_path, chapters_path
+
+
+def write_description_file(path, yt):
+    with open(f"{path}.description.txt", 'w+b') as fo:
+        fo.write(f"From: {yt.watch_url}\n\nDescription:\n\n".encode())
+        fo.write(yt.description.encode())
+
+
+def get_chapters(chapters_str: str) -> list:
+    # Read the description file
+    # Split into time and chapter name
+
+    list_of_chapters = []
+
+    # only increment chapter number on a chapter line
+    # chapter lines start with timecode
+    line_counter = 1
+    for line in chapters_str.split('\n'):
+        result = re.search(r"\(?(\d?[:]?\d+[:]\d+)\)?", line)
+        try:
+            # result = re.search("\(?(\d+[:]\d+[:]\d+)\)?", line)
+            time_count = datetime.datetime.strptime(result.group(1), '%H:%M:%S')
+        except:
+            try:
+                # result = re.search("\(?(\d+[:]\d+)\)?", line)
+                time_count = datetime.datetime.strptime(result.group(1), '%M:%S')
+            except:
+                continue
+        chap_name = line.replace(result.group(0), "").rstrip(' :\n')
+        chap_pos = datetime.datetime.strftime(time_count, '%H:%M:%S')
+        list_of_chapters.append((str(line_counter).zfill(2), chap_pos, chap_name))
+        line_counter += 1
+
+    return list_of_chapters
+
+
+def write_chapters_file(chapter_file: str, chapter_list: list) -> None:
+    # open(chapter_file, 'w').close()
+
+    # Write out the chapter file based on simple MP4 format (OGM)
+    with open(chapter_file, 'w') as fo:
+        for current_chapter in chapter_list:
+            fo.write(f'CHAPTER{current_chapter[0]}='
+                     f'{current_chapter[1]}\n'
+                     f'CHAPTER{current_chapter[0]}NAME='
+                     f'{current_chapter[2]}\n')
+
+
+def add_chapters_to_mp4(chapter_file_name: str, name_for_download: str) -> None:
+    # Use MP4Box to mux the chapter file with the mp4
+    subprocess.run(["MP4Box", "-chap", chapter_file_name, name_for_download])
 
 
 def main() -> None:
-    audio_path, video_path = download_streams(sys.argv[1])
+    audio_path, video_path, chapters_path = download_streams(sys.argv[1])
 
     dest_video_path: str = f"{video_path.split(".")[0]} - muxed.mp4"
 
-    video_audio_mux(audio_path, video_path, dest_video_path)
+    mux_files(audio_path, video_path, chapters_path, dest_video_path)
 
     delete_file(audio_path)
     delete_file(video_path)
+
+    if chapters_path:
+        delete_file(chapters_path)
 
     os.rename(dest_video_path, video_path)
 
