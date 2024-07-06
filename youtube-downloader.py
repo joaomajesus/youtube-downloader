3077d7dc24275922ebca957cef2c4eb0a73a9475
João Jesus <joaomajesus@gmail.com>
2024-07-06 13:44:45 +0100

    fix formating

--- youtube-downloader.py
+++ youtube-downloader.py
@@ -7,7 +7,12 @@ import re
 import subprocess
 
 
-def mux_files(audio_source_spath: str, image_source_path: str, chapters_path: str, out_video_path: str) -> None:
+def mux_files(
+    audio_source_spath: str,
+    image_source_path: str,
+    chapters_path: str,
+    out_video_path: str,
+) -> None:
     """
     Muxes audio and video files into a single output video.
 
@@ -22,7 +27,7 @@ def mux_files(audio_source_spath: str, image_source_path: str, chapters_path: st
     """
     video = ffmpeg.input(image_source_path).video
     audio = ffmpeg.input(audio_source_spath).audio
-    ffmpeg.output(audio, video, out_video_path, vcodec='copy', acodec='copy').run()
+    ffmpeg.output(audio, video, out_video_path, vcodec="copy", acodec="copy").run()
 
     if chapters_path:
         add_chapters_to_mp4(chapters_path, out_video_path)
@@ -63,29 +68,24 @@ def download_streams(url: str) -> (str, str, str):
     chapters_path = ""
     yt = YouTube(url)
 
-    video_path = (yt
-                  .streams
-                  .filter(progressive=False,
-                          file_extension='mp4')
-                  .order_by('resolution')
-                  .desc()
-                  .first()
-                  .download(output_path=output_path))
+    video_path = (
+        yt.streams.filter(progressive=False, file_extension="mp4")
+        .order_by("resolution")
+        .desc()
+        .first()
+        .download(output_path=output_path)
+    )
 
-    audio_stream = (yt
-                    .streams
-                    .filter(progressive=False,
-                            only_audio=True,
-                            audio_codec="mp4a.40.2")
-                    .order_by('bitrate')
-                    .desc()
-                    .first())
+    audio_stream = (
+        yt.streams.filter(progressive=False, only_audio=True, audio_codec="mp4a.40.2")
+        .order_by("bitrate")
+        .desc()
+        .first()
+    )
 
     path = video_path.split(".")[0]
 
-    audio_path = (audio_stream
-                  .download(filename=f"{path}.m4a",
-                            output_path=output_path))
+    audio_path = audio_stream.download(filename=f"{path}.m4a", output_path=output_path)
 
     write_description_file(path, yt)
 
@@ -109,7 +109,7 @@ def write_description_file(path: str, yt: YouTube):
     Returns:
         None
     """
-    with open(f"{path}.txt", 'w+b') as fo:
+    with open(f"{path}.txt", "w+b") as fo:
         fo.write(f"From: {yt.watch_url}\n\nDescription:\n\n".encode())
         fo.write(yt.description.encode())
 
@@ -127,19 +127,19 @@ def get_chapters(description: str) -> list:
     list_of_chapters = []
 
     line_counter = 1
-    for line in description.split('\n'):
+    for line in description.split("\n"):
         result = re.search(r"\(?(\d?:?\d+:\d+)\)?", line)
 
         try:
-            time_count = datetime.datetime.strptime(result.group(1), '%H:%M:%S')
+            time_count = datetime.datetime.strptime(result.group(1), "%H:%M:%S")
         except:
             try:
-                time_count = datetime.datetime.strptime(result.group(1), '%M:%S')
+                time_count = datetime.datetime.strptime(result.group(1), "%M:%S")
             except:
                 continue
 
-        chap_name = line.replace(result.group(0), "").rstrip(' :\n')
-        chap_pos = datetime.datetime.strftime(time_count, '%H:%M:%S')
+        chap_name = line.replace(result.group(0), "").rstrip(" :\n")
+        chap_pos = datetime.datetime.strftime(time_count, "%H:%M:%S")
         list_of_chapters.append((str(line_counter).zfill(2), chap_pos, chap_name))
         line_counter += 1
 
@@ -149,17 +149,19 @@ def get_chapters(description: str) -> list:
 def write_chapters_file(chapter_file_path: str, chapters: list) -> None:
     """
     Write out the chapter file based on simple MP4 format (OGM).
+
     Args:
         chapter_file_path (str): The path to the chapter file.
         chapters (list): A list of tuples containing chapter information.
             Each tuple should have the following structure: (chapter_number, chapter_position, chapter_name).
+
     Returns:
         None
     """
-    with open(chapter_file_path, 'w+b') as fo:
+    with open(chapter_file_path, "w+b") as fo:
         for current_chapter in chapters:
-            fo.write(f'CHAPTER{current_chapter[0]}={current_chapter[1]}\n'.encode())
-            fo.write(f'CHAPTER{current_chapter[0]}NAME={current_chapter[2]}\n'.encode())
+            fo.write(f"CHAPTER{current_chapter[0]}={current_chapter[1]}\n".encode())
+            fo.write(f"CHAPTER{current_chapter[0]}NAME={current_chapter[2]}\n".encode())
 
 
 def add_chapters_to_mp4(chapter_file_path: str, mp4_file_path: str) -> None:
